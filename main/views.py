81d11d62f787259eac05242a1ff387d996bbebf0
codaaa19 <tsabit.coda@ui.ac.id>
2023-11-15 16:56:56 +0700

    Tutorial 8

--- main/views.py
+++ main/views.py
@@ -16,6 +16,8 @@ import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt;
+from django.http import JsonResponse
+import json
 
 @login_required(login_url='/login')
 def show_main(request):
@@ -137,3 +139,21 @@ def add_product_ajax(request):
     
     return HttpResponseNotFound()
 
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+
+        new_product = Product.objects.create(
+            user = request.user,
+            name = data["name"],
+            price = int(data["price"]),
+            description = data["description"]
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
